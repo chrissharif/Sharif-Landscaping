18141fb98a1b8e00dab000dd939a71dcee0f22cb
Christopher Sharif <christophersharif@gmail.com>
2023-04-16 10:19:48 -0400

    Added Email Form

--- sharif-landscaping/src/components/Contact.jsx
+++ sharif-landscaping/src/components/Contact.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    emailjs.sendForm('service_8m4m11o', 'template_pyszqua', form.current, 'h32hmB8tPU-nBHM5k')
+      .then((result) => {
+      }, (error) => {
+    });
+  };
+  
+  
   return (
-    <div>
-      <div>contact us</div>
+    <div className='contact-container'>
+      <div className='contact-info'>
+        For any questions, concerns, or if you're looking
+        to use our services, fill out the form below to
+        contact us!
+      </div>
+      <div className='contact-form-container'>
+        <form ref={form} onSubmit={sendEmail} className='contact-form'>
+          <label for="name">Name:</label>
+          <input type="text" name='user_name' className='contact-input'/>
+          <label for="phone">Phone:</label>
+          <input type="text" name='user_phone' className='contact-input'/>
+          <label for="email">Email:</label>
+          <input type="text" name='user_email' className='contact-input'/>
+          <label for="message">Message:</label>
+          <textarea name='message' cols="30" rows="7" className='contact-input'></textarea>
+          <button type="submit" value='Send' className='submit-button'>Send Message</button>
+        </form>
+      </div>         
     </div>
   )
 }
